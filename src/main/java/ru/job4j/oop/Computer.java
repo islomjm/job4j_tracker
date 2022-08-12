4964357960e454afc660b1ab5a9c861ff9743bc9
islom <islomnizom@gmail.com>
2022-08-12 15:36:23 +0300

    Пример из урока 1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -0,0 +1,35 @@
+package ru.job4j.oop;
+
+public class Computer {
+
+    private boolean multiMonitor;
+    private int ssd;
+    private String cpu;
+    private String gpu;
+    public void printInfo() {
+        System.out.println("Много мониторов: " + multiMonitor);
+        System.out.println("SSD: " + ssd + " GB");
+        System.out.println("Модель CPU: " + cpu);
+        System.out.println("Модель GPU: " + gpu);
+    }
+
+    public Computer() {
+    }
+    public Computer(boolean multiMonitor, int ssd, String cpu, String gpu) {
+        this.multiMonitor = multiMonitor;
+        this.ssd = ssd;
+        this.cpu = cpu;
+        this.gpu = gpu;
+    }
+
+    public static void main(String[] args) {
+        Computer computer = new Computer(true, 500, "Intel Core I7-10700k", "Nvidia RTX 1050");
+        computer.printInfo();
+        System.out.println();
+        Computer info = new Computer(true, 1024, "AMD Ryzen 7 5800H", "Nvidia RTX 3070");
+        info.printInfo();
+        Computer copm = new Computer();
+        System.out.println();
+        copm.printInfo();
+    }
+}
