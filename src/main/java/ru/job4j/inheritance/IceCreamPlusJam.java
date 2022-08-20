28ee95d5bae9ffeb884c3717790bd794b8641adc
islom <islomnizom@gmail.com>
2022-08-20 17:31:16 +0300

    Example from lesson: 5. Вызов переопределенного метода родителя - super.method ... [#504871]

---
+++ src/main/java/ru/job4j/inheritance/IceCreamPlusJam.java
@@ -0,0 +1,13 @@
+package ru.job4j.inheritance;
+
+public class IceCreamPlusJam extends IceCream {
+    private static final int JAM_PRICE = 50;
+
+    public IceCreamPlusJam(int weight) {
+        super(weight);
+    }
+
+    public int price() {
+        return JAM_PRICE + super.price();
+    }
+}
