9c688d68e3813084b8a2cdb4c40b1c0c453fc4ad
islom <islomnizom@gmail.com>
2022-08-21 14:55:31 +0300

    Example for lesson  2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/Police.java
@@ -0,0 +1,15 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class Police {
+    public static void main(String[] args) {
+        License license = new License();
+        license.setOwner("Jambaev Islomjon");
+        license.setModel("Mazda");
+        license.setCode("B669EB134");
+        license.setCreated(new Date());
+
+        System.out.println(license.getOwner() + " has a car - " + license.getModel() + " : " + license.getCode());
+    }
+}
